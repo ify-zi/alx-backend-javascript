f8174008cb713e91dc144911bba3ec510a94179b
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 10:51:06 +0000

    task #0, #1 and #2

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromApi(success) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (success === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
