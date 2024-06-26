df1b7917861329b9aa5245c0838d350ba444066d
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 11:13:58 +0000

    correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function getFullResponseFromApi(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .finally(() => console.log('Got a response from Api'));
+    .finally(() => console.log('Got a response from API'));
 }
