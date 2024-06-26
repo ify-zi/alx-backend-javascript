2460d332bdb8b90619076208293174360bcd94c5
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 11:20:44 +0000

    error in the function naming

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromApi(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .finally(() => console.log('Got a response from API'));
+    .finally(() => console.log('Got a response from the API'));
 }
