6cb65d34782d914a85ce05be66156726a967fcf9
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 11:14:57 +0000

    all in promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstname} ${value[2].lastname}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
