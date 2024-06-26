1c72e0692b3a683a4d34b1124e571bfc1172c93d
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 12:01:26 +0000

    chaining promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((values) => {
+      const arr = [];
+      for (const val of values) {
+        arr.push({ status: val.status, value: val.value || val.reason });
+      }
+      return arr;
+    });
+}
