1f12eae8fd796aedeb6de5bf1478045b4e796dbf
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 11:37:25 +0000

    #4 handled

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,14 +1,13 @@
 export default function signUpUser(firstName, lastName) {
-  const object =
-		{
-			firstName,
-			lastName
-		};
-	return new Promise((resolve, reject) => {
-		if (firstName && lastName) {
-			resolve(object);
-		} else {
-			reject(Error('error'));
-		}
-	});
+  const object = {
+    firstName,
+    lastName,
+  };
+  return new Promise((resolve, reject) => {
+    if (firstName && lastName) {
+      resolve(object);
+    } else {
+      reject(Error('error'));
+    }
+  });
 }
