0564640d8d2d5658a58a9c88db8a1c849f8aa94c
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-06-26 20:55:20 +0000

    debugging please work na

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ])
-    .then((values) => {
-      const arr = [];
-      for (const val of values) {
-        arr.push({ status: val.status, value: val.value || val.reason });
-      }
-      return arr;
-    });
+  ]).then((values) => {
+    const arr = [];
+    for (const val of values) {
+      arr.push({ status: val.status, value: val.value || val.reason });
+    }
+    return arr;
+  });
 }
