c01df270426c50ce784170a95447900d319a2e52
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-03 12:18:45 +0000

    task #1 done

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,49 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+  }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+interface printTeacherfunction {
+  (firstName: string, lastName: string): string;
+}
+
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
